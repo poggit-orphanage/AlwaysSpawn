48ef658e18451f4b2542fa8a6bdda995eb76c746
Comedyman937 <comedyman937@yahoo.com>
2013-12-14 15:17:52 -0500

    Create AlwaysSpawn.php

---
+++ AlwaysSpawn.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+__PocketMine Plugin__
+name=AlwaysSpawn
+description=Force your users to automatically spawn every time they login!
+version=1.0.0
+author=Comedyman937
+class=Spawner
+apiversion=6,7,8,9,10,11,12,13,14,15,16,17
+*/
+
+    class Spawner implements plugin{
+
+    private $api;
+
+    public function __construct(ServerAPI $api, $server = false){
+
+		$this->api = $api;
+
+	}
+
+	public function init(){
+
+    $this->api->addHandler("player.connect", array($this, "eventHandler"), 100);
+
+    }
+    
+    public function eventHandler($data, $event)
+    {
+    switch($event)
+    {
+        case 'player.connect':
+        
+            $this->api->console->run("spawn");
+        
+        break;
+    }
+
+    }
+
+    public function __destruct(){
+
+    }
+
+}
