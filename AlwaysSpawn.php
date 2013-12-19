74dcb7b13794ad96b3606743bed48eafacea0e3c
Comedyman937 <comedyman937@yahoo.com>
2013-12-19 18:22:02 -0500

    MANY Bug Fixes

--- AlwaysSpawn.php
+++ AlwaysSpawn.php
@@ -7,36 +7,40 @@ description=Force your users to automatically spawn every time they login!
 version=1.0.3
 author=Comedyman937
 class=Spawner
-apiversion=6,7,8,9,10,11,12,13,14,15,16,17
+apiversion=6,7,8,9,10,11
 */
 
     class Spawner implements plugin{
 
     private $api;
 
-    public function __construct(ServerAPI $api, $server = false){
+    public function __construct(ServerAPI $api, $server = false)
+    {
+        $this->api = $api;
+    }
 
-		$this->api = $api;
-
-	}
-
-	public function init(){
-
-        $this->api->addHandler("player.connect", array($this, "eventHandler"), 100);
-
+    public function init()
+    {
+        $this->api->addHandler("player.spawn", array($this, "eventHandler"), 100);
     }
     
     public function eventHandler($data, $event)
     {
-    switch($event)
-    {
-        case 'player.connect':
+        switch($event)
+        {
+            case 'player.spawn':
 
-            $data['player']->run("spawn");
+                // PocketMine core schedules lots of teleports - ugh, we need to wait awhile
+                $this->api->schedule(31, array($this, "teleport"), $data);
 
-        break;
+            break;
+        }
+
     }
 
+    public function teleport($player)
+    {
+         $player->teleport($this->api->level->getSpawn());
     }
 
     public function __destruct(){
