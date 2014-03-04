b5b876eedf7ca9bd2b654199162eeef489e3b6bf
Comedyman937 <comedyman937@yahoo.com>
2014-03-04 10:54:25 -0500

    2.0.1 Development Build #3

--- Plugin/v2.0.1/AlwaysSpawn.php
+++ Plugin/v2.0.1/AlwaysSpawn.php
@@ -1,2 +1,85 @@
-//I think I have patched the server crash bug for this version of AlwaysSpawn
-//However I have yet to test it... Please check back later!
+<?php
+/*
+__PocketMine Plugin__
+name=AlwaysSpawn
+description=Force your users to automaticaly spawn every time they login!
+version=2.0.1dev
+author=Comedyman937
+class=AlwaysSpawn
+apiversion=6,7,8,9,10,11,12
+*/
+class AlwaysSpawn implements Plugin{
+    private $api, $path, $config;
+
+    public function __construct(ServerAPI $api, $server = false)
+    {
+        $this->api = $api;
+    }
+
+    public function init()
+    {
+        $this->config = new Config($this->api->plugin->configPath($this)."config.yml", CONFIG_YAML, array(
+            'enableConf' => false,
+            'spawnWorld' => 'world',
+            'X' => 0,
+            'Y' => 0,
+            'Z' => 0,
+        ));
+        $this->api->console->register("aslocation", "Get your in game location!", array($this, "location"));
+        $this->api->addHandler("player.spawn", array($this, "eventHandler"), 100);
+        console("[INFO] AlwaysSpawn Loaded!");
+    }
+
+    public function eventHandler($data, $event)
+    {
+        $enableConf=$this->config->get('enableConf');
+        if($enableConf=true){
+            console("[INFO] [AlwaysSpawn] 'enableConf' is set to true in the config.yml file!");
+            switch($event){
+                case 'player.spawn':
+                    $XPos=$this->config->get('X');
+                    $YPos=$this->config->get('Y');
+                    $ZPos=$this->config->get('Z');
+                    $Level=$this->config->get('spawnWorld');
+                    $username=$data;
+                    $spawn=new Position($XPos, $YPos, $ZPos, $Level);
+                    $this->api->player->teleport($username, $spawn);
+                break;
+            }
+        }elseif($enableConf=false){
+            console("[INFO] [AlwaysSpawn] 'enableConf' is set to true in the config.yml file!");
+            switch($event){
+                case 'player.spawn':
+                    $this->api->shedule(31, array($this, "teleport"), $data);
+                break;
+            }
+        }else{
+            console("[ERROR] [AlwaysSpawn] The AlwaysSpawn config.yml file is corrupt!  Stopping server to prevent damage...");
+            $this->api->console->run("stop");
+        }
+    }
+   
+    public function teleport($player)
+    {
+        $player->teleport($this->api->level->getSpawn());
+    }
+
+    public function location($data, $issuer, $event)
+    {
+         switch($event){
+              case "aslocation":
+                   if(!($issuer instanceof Player)){
+                        $output = "[AlwaysSpawn] Please run this command in-game!";
+                   }else{
+                        $output = "====================\nLocation:\nX: ".ceil($player->entity->x)."\nY: ".ceil($player->entity->y)."\nZ: ".ceil($player->entity->z)."\nWorld: ".$player->entity->level->getName()."\n====================";
+              }
+                   return $output;
+              break;
+         }
+    }
+   
+    public function __destruct(){
+        console("[INFO] AlwaysSpawn Unloaded!");
+    }
+}
+?>
