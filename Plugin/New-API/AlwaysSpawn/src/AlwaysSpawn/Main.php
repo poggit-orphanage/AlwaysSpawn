7c3c3f1a8e160b699317d75149a856c2d0911008
Philip Shilling <comedyman937@yahoo.com>
2014-06-05 15:32:18 -0500

    Started API Update

---
+++ Plugin/New-API/AlwaysSpawn/src/AlwaysSpawn/Main.php
@@ -0,0 +1,18 @@
+<php
+
+namespace AlwaysSpawn;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+
+class Main extends PluginBase{
+
+    public function onEnable(){
+    }
+    
+    public function onConnect(){
+    }
+    
+    public function onDisable(){
+    }
+}
