5788dcdcdebbe4c53c2fefdfaf28b5718f4570ec
Philip Shilling <comedyman937@yahoo.com>
2014-06-05 21:54:00 -0500

    Finished A Few Things
    AlwaysSpawn 2.1.0 Should Now Be Ready For Testing!
    
    I might be forgetting a few pocketmine uses though.

--- Plugin/New-API/AlwaysSpawn/src/AlwaysSpawn/Main.php
+++ Plugin/New-API/AlwaysSpawn/src/AlwaysSpawn/Main.php
@@ -3,14 +3,17 @@
 namespace AlwaysSpawn;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
 use pocketmine\command\PluginCommand;
 use pocketmine\command\CommandSender;
 use pocketmine\command\ConsoleCommandSender;
 use pocketmine\command\RemoteConsoleCommandSender;
+use pocketmine\command\CommandExecutor;
+use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\Player;
 
-class Main extends PluginBase{
+class Main extends PluginBase implements Listener, CommandExecutor{
 
     public function onEnable(){
         $cmd = new PluginCommand("alwaysspawn", "onCommand");
@@ -18,17 +21,29 @@ class Main extends PluginBase{
         $cmd->setDescription("Admin commands for AlwaysSpawn!");
         $cmd->setAliases(array("as"));
         
-        $this->getServer()->getPluginManager()->registerEvents("PlayerJoinEvent", onJoin);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
         
         $this->getLogger()->log("[INFO] AlwaysSpawn Loaded!");
     }
     
-    public function onJoin(pocketmine\event\player\PlayerJoinEvent $playerjoin){
-        
+    /*
+     * @param PlayerJoinEvent $event
+     *
+     * @priority       NORMAL
+     * @ignoreCanceled false
+     */
+    public function onSpawn(PlayerJoinEvent $event){
+        $player->teleport($this->level->getSpawn());
     }
     
-    public function onCommand(pocketmine\command\CommandSender $issuer, Command $cmd, $label, array $args){
-        $issuer->sendMessage("[AlwaysSpawn] Commands are not currently accessable with the New API!\nThey will be re-aded in a later update!");
+    public function onCommand(CommandSender $issuer, Command $cmd, $label, array $args){
+        switch($cmd->getName){
+            case "alwaysspawn":
+                $issuer->sendMessage("[AlwaysSpawn] Commands have not yet been implemented for the New API!");
+                return true;
+            default:
+                return false;
+        }
     }
     
     public function onDisable(){
