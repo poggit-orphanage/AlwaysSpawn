751715d7e39b127d3b3875e051892f39a65cf561
Philip Shilling <comedyman937@yahoo.com>
2014-06-06 19:35:19 -0500

    Trying to fix errors

--- Plugin/New-API/AlwaysSpawn/src/AlwaysSpawn/Main.php
+++ Plugin/New-API/AlwaysSpawn/src/AlwaysSpawn/Main.php
@@ -3,28 +3,16 @@
 namespace AlwaysSpawn;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\command\Command;
-use pocketmine\command\PluginCommand;
-use pocketmine\command\CommandSender;
-use pocketmine\command\ConsoleCommandSender;
-use pocketmine\command\RemoteConsoleCommandSender;
-use pocketmine\command\CommandExecutor;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\level\Level;
 use pocketmine\entity\Entity;
 use pocketmine\Player;
 
-class Main extends PluginBase implements Listener, CommandExecutor{
+class Main extends PluginBase implements Listener{
 
     public function onEnable(){
-        $cmd = new PluginCommand("alwaysspawn", "onCommand");
-        
-        $cmd->setDescription("Admin commands for AlwaysSpawn!");
-        $cmd->setAliases(array("as"));
-        
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        
         $this->getLogger()->log("[INFO] AlwaysSpawn Loaded!");
     }
     
@@ -36,16 +24,6 @@ class Main extends PluginBase implements Listener, CommandExecutor{
      */
     public function onSpawn(PlayerJoinEvent $event){
         $player->teleport($this->level->getSpawn());
-    }
-    
-    public function onCommand(CommandSender $issuer, Command $cmd, $label, array $args){
-        switch($cmd->getName){
-            case "alwaysspawn":
-                $issuer->sendMessage("[AlwaysSpawn] Commands have not yet been implemented for the New API!");
-                return true;
-            default:
-                return false;
-        }
     }
     
     public function onDisable(){
