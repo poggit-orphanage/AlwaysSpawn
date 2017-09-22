d0008a92fc3ad0cc44bbc7360f78dff04a878f44
jazzwhistle <jdcraftmcpe@gmail.com>
2017-09-22 14:23:45 +0200

    Fix for no perms after login

--- src/philipshilling/alwaysspawn/Loader.php
+++ src/philipshilling/alwaysspawn/Loader.php
@@ -4,7 +4,7 @@
 	use pocketmine\level\Position;
     use pocketmine\plugin\PluginBase as Plugin;
 	use pocketmine\event\Listener;
-	use pocketmine\event\player\PlayerLoginEvent;
+	use pocketmine\event\player\PlayerJoinEvent;
 				
 	class Loader extends Plugin implements Listener {
 		public function onEnable() {
@@ -12,13 +12,12 @@
 			$this->getServer()->getLogger()->info("AlwaysSpawn Enabled!");
 		}
 		
-		public function onPlayerLogin(PlayerLoginEvent $event) {
+		public function onPlayerLogin(PlayerJoinEvent $event) {
 			$player = $event->getPlayer();
 			$x = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getX();
 			$y = $this->getServer()->getDefaultLevel()->getSafeSpawn()-> getY();
 			$z = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getZ();
 			$level = $this->getServer()->getDefaultLevel();
-			$player->setLevel($level);
 			$player->teleport(new Position($x, $y, $z, $level));
 		}
 
