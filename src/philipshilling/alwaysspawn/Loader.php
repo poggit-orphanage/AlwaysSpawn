40a191a11fec1257bcad66abb74f1a16a12358fc
Dylan K. Taylor <odigiman@gmail.com>
2017-09-25 15:40:23 +0100

    Fix disgraceful formatting

--- src/philipshilling/alwaysspawn/Loader.php
+++ src/philipshilling/alwaysspawn/Loader.php
@@ -1,24 +1,25 @@
 <?php
-	namespace philipshilling\alwaysspawn;
-	
-	use pocketmine\level\Position;
-    use pocketmine\plugin\PluginBase as Plugin;
-	use pocketmine\event\Listener;
-	use pocketmine\event\player\PlayerJoinEvent;
-				
-	class Loader extends Plugin implements Listener {
-		public function onEnable() {
-			$this->getServer()->getPluginManager()->registerEvents($this, $this);
-			$this->getServer()->getLogger()->info("AlwaysSpawn Enabled!");
-		}
-		
-		public function onPlayerLogin(PlayerJoinEvent $event) {
-			$player = $event->getPlayer();
-			$x = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getX();
-			$y = $this->getServer()->getDefaultLevel()->getSafeSpawn()-> getY();
-			$z = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getZ();
-			$level = $this->getServer()->getDefaultLevel();
-			$player->teleport(new Position($x, $y, $z, $level));
-		}
 
+namespace philipshilling\alwaysspawn;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\level\Position;
+use pocketmine\plugin\PluginBase as Plugin;
+
+class Loader extends Plugin implements Listener{
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getServer()->getLogger()->info("AlwaysSpawn Enabled!");
 	}
+
+	public function onPlayerLogin(PlayerJoinEvent $event){
+		$player = $event->getPlayer();
+		$x = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getX();
+		$y = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getY();
+		$z = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getZ();
+		$level = $this->getServer()->getDefaultLevel();
+		$player->teleport(new Position($x, $y, $z, $level));
+	}
+
+}
