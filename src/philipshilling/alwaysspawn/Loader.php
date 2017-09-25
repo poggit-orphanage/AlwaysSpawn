8fe336147962468e00bc090dc6e1bcd4a1b1476b
Dylan K. Taylor <odigiman@gmail.com>
2017-09-25 15:49:00 +0100

    Fixed pointless overcomplication

--- src/philipshilling/alwaysspawn/Loader.php
+++ src/philipshilling/alwaysspawn/Loader.php
@@ -4,7 +4,6 @@ namespace philipshilling\alwaysspawn;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\level\Position;
 use pocketmine\plugin\PluginBase as Plugin;
 
 class Loader extends Plugin implements Listener{
@@ -14,12 +13,7 @@ class Loader extends Plugin implements Listener{
 	}
 
 	public function onPlayerLogin(PlayerJoinEvent $event){
-		$player = $event->getPlayer();
-		$x = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getX();
-		$y = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getY();
-		$z = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getZ();
-		$level = $this->getServer()->getDefaultLevel();
-		$player->teleport(new Position($x, $y, $z, $level));
+		$event->getPlayer()->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
 	}
 
 }
