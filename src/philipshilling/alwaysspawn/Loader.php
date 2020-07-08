6baee56b730785bbd65b0456af748924e1329785
suerion <55131959+suerion@users.noreply.github.com>
2020-07-08 17:24:31 +0200

    fixed the bug, players spawn in void worlds
    changed the playerlogin event to player join event to fix the problem with proxys

--- src/philipshilling/alwaysspawn/Loader.php
+++ src/philipshilling/alwaysspawn/Loader.php
@@ -4,16 +4,16 @@ namespace philipshilling\alwaysspawn;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\plugin\PluginBase as Plugin;
+use pocketmine\Server;
 
 class Loader extends Plugin implements Listener{
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->getServer()->getLogger()->info("AlwaysSpawn Enabled!");
 	}
-
-	public function onPlayerLogin(PlayerLoginEvent $event){
+	public function onPlayerJoin(PlayerJoinEvent $event) {
 		$event->getPlayer()->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
 	}
-
 }
