77bd5183321b9d7148a0a36c94bdd15e9a240ce1
suerion <55131959+suerion@users.noreply.github.com>
2020-07-11 20:15:08 +0200

    Update Loader.php

--- src/philipshilling/alwaysspawn/Loader.php
+++ src/philipshilling/alwaysspawn/Loader.php
@@ -9,14 +9,12 @@ use pocketmine\Server;
 use alwaysspawn\ResourceManager;
 use alwaysspawn\ResourceUpdater;
 
-class Loader extends Plugin implements Listener
-{
+class Loader extends Plugin implements Listener {
 
     public $resourceManager = null;
     public $resourceUpdater = null;
 
-    public function onEnable()
-    {
+    public function onEnable() {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getServer()->getLogger()->info("AlwaysSpawn Enabled!");
         $this->resourceManager = ResourceManager::getInstance($this, $this->getServer());
@@ -24,16 +22,14 @@ class Loader extends Plugin implements Listener
         $this->resourceUpdater = ResourceUpdater::getInstance($this->resourceManager);
         $this->resourceUpdater->updateResourcesIfRequired(true);
     }
-    public function onPlayerJoin(PlayerJoinEvent $eventjoin)
-    {
+    public function onPlayerJoin(PlayerJoinEvent $eventjoin) {
         if ($this->resourceManager->getConfig()["alwaysspawn with proxy"] === true) {
             $this->getLogger()->info("Alwaysspawn starts for proxy setting");
         }
         return $eventjoin->getPlayer()->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
     }
 
-    public function onPlayerLogin(PlayerLoginEvent $eventlogin)
-    {
+    public function onPlayerLogin(PlayerLoginEvent $eventlogin) {
         if ($this->resourceManager->getConfig()["alwaysspawn with proxy"] === false) {
             $this->getLogger()->info("Alwaysspawn starts without proxy setting");    
         }
