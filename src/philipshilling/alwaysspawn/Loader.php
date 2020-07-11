7b1a80c5727be0a5c54898523b32d09cae888867
suerion <55131959+suerion@users.noreply.github.com>
2020-07-11 19:34:45 +0200

    addes resourcemanager and updater,
    if in config with proxy, you could add true, if not false

--- src/philipshilling/alwaysspawn/Loader.php
+++ src/philipshilling/alwaysspawn/Loader.php
@@ -1,19 +1,50 @@
 <?php
-
 namespace philipshilling\alwaysspawn;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerLoginEvent;
-use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\plugin\PluginBase as Plugin;
 use pocketmine\Server;
+use ResourceManager;
+use ResourceUpdater;
 
-class Loader extends Plugin implements Listener{
-	public function onEnable(){
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->getServer()->getLogger()->info("AlwaysSpawn Enabled!");
-	}
-	public function onPlayerJoin(PlayerJoinEvent $event) {
-		$event->getPlayer()->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
-	}
+class Loader extends Plugin implements Listener
+{
+
+    public $resourceManager = null;
+
+    public $resourceUpdater = null;
+
+    public function onEnable()
+    {
+        $this->getServer()
+            ->getPluginManager()
+            ->registerEvents($this, $this);
+        $this->getServer()
+            ->getLogger()
+            ->info("AlwaysSpawn Enabled!");
+
+        $this->resourceManager = ResourceManager::getInstance($this, $this->getServer());
+        $this->resourceManager->loadResources();
+        $this->resourceUpdater = ResourceUpdater::getInstance($this->resourceManager);
+        $this->resourceUpdater->updateResourcesIfRequired(true);
+    }
+
+    public function onPlayerJoin(PlayerJoinEvent $event)
+    {
+        if ($this->resourceManager->getConfig()["alwaysspawn with proxy"] === false) {
+            $event->getPlayer()->teleport($this->getServer()
+                ->getDefaultLevel()
+                ->getSafeSpawn());
+        }
+    }
+
+    public function onPlayerLogin(PlayerLoginEvent $event)
+    {
+        if ($this->resourceManager->getConfig()["alwaysspawn with proxy"] === true) {
+            $event->getPlayer()->teleport($this->getServer()
+                ->getDefaultLevel()
+                ->getSafeSpawn());
+        }
+    }
 }
---
+++ src/philipshilling/alwaysspawn/ResourceManager.php
@@ -0,0 +1,73 @@
+<?php
+namespace philipshilling\alwaysspawn\resourcemanager;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+
+class ResourceManager
+{
+
+    private static $instance = null;
+
+    private $resUpdaterInstance = null;
+
+    private $pluginInstance = null;
+
+    private $serverInstance = null;
+
+    private $pluginVersion = null;
+
+    private $config = [];
+
+    private function __construct(alwaysspawn $plugin, Server $sv)
+    {
+        $this->pluginInstance = $plugin;
+        $this->serverInstance = $sv;
+        $this->resUpdaterInstance = ResourceUpdater::getInstance($this);
+
+        $this->pluginVersion = $this->pluginInstance->getDescription()->getVersion();
+    }
+
+    public function getConfig()
+    {
+        return $this->config;
+    }
+
+    public function getConfigVersion()
+    {
+        if (isset($this->config['version']))
+            return $this->config['version'];
+
+        return null;
+    }
+
+    public function loadResources()
+    {
+        if (! is_dir($path = $this->pluginInstance->getDataFolder())) {
+            mkdir($path);
+        }
+        $this->loadConfig($path);
+    }
+
+    public function loadConfig($path)
+    {
+        /**
+         * load config if file exists and if not create a file
+         */
+        if (is_file($path . 'config.yml')) {
+            $this->config = yaml_parse_file($path . 'config.yml');
+        } else {
+            $this->config = $this->resUpdaterInstance->getDefaultConfig();
+
+            yaml_emit_file($path . 'config.yml', $this->config);
+        }
+    }
+
+    public function saveConfig($config)
+    {
+        $this->config = $config;
+
+        $path = $this->pluginInstance->getDataFolder();
+        yaml_emit_file($path . 'config.yml', $this->config);
+    }
+}
