1060d561efa6d48fe7bf41b8cdd1883f6430b937
suerion <55131959+suerion@users.noreply.github.com>
2020-07-11 19:39:30 +0200

    Update Loader.php

--- src/philipshilling/alwaysspawn/Loader.php
+++ src/philipshilling/alwaysspawn/Loader.php
@@ -3,6 +3,7 @@ namespace philipshilling\alwaysspawn;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\plugin\PluginBase as Plugin;
 use pocketmine\Server;
 use ResourceManager;
@@ -17,13 +18,8 @@ class Loader extends Plugin implements Listener
 
     public function onEnable()
     {
-        $this->getServer()
-            ->getPluginManager()
-            ->registerEvents($this, $this);
-        $this->getServer()
-            ->getLogger()
-            ->info("AlwaysSpawn Enabled!");
-
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getLogger()->info("AlwaysSpawn Enabled!");
         $this->resourceManager = ResourceManager::getInstance($this, $this->getServer());
         $this->resourceManager->loadResources();
         $this->resourceUpdater = ResourceUpdater::getInstance($this->resourceManager);
@@ -33,18 +29,14 @@ class Loader extends Plugin implements Listener
     public function onPlayerJoin(PlayerJoinEvent $event)
     {
         if ($this->resourceManager->getConfig()["alwaysspawn with proxy"] === false) {
-            $event->getPlayer()->teleport($this->getServer()
-                ->getDefaultLevel()
-                ->getSafeSpawn());
+            $event->getPlayer()->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
         }
     }
 
     public function onPlayerLogin(PlayerLoginEvent $event)
     {
         if ($this->resourceManager->getConfig()["alwaysspawn with proxy"] === true) {
-            $event->getPlayer()->teleport($this->getServer()
-                ->getDefaultLevel()
-                ->getSafeSpawn());
+            $event->getPlayer()->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
         }
     }
 }
